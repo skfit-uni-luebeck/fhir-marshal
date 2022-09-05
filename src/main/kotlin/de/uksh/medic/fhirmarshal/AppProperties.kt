bfb9513740a60becb5d8e5571b1065a0893115b5
Joshua Wiedekopf <jpwiedekopf@gmail.com>
2022-09-05 17:26:58 +0200

    support for multiple servers and basic auth

--- src/main/kotlin/de/uksh/medic/fhirmarshal/AppProperties.kt
+++ src/main/kotlin/de/uksh/medic/fhirmarshal/AppProperties.kt
@@ -1,13 +1,43 @@
 package de.uksh.medic.fhirmarshal
 
+import ca.uhn.fhir.context.FhirContext
+import ca.uhn.fhir.rest.client.interceptor.BasicAuthInterceptor
+import org.hl7.fhir.common.hapi.validation.support.RemoteTerminologyServiceValidationSupport
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
+import org.springframework.http.client.support.BasicAuthenticationInterceptor
+import org.springframework.web.client.RestTemplate
 import java.net.URI
 
 @ConfigurationProperties(prefix = "fhir")
 @ConstructorBinding
 data class AppProperties(
-    val remoteStructureServer: URI,
-    val remoteTerminologyServer: URI,
+    val remoteStructureServers: Map<String, ServerSettings>,
+    val remoteTerminologyServers: Map<String, ServerSettings>,
     val retrievalPageSize: Int = 3
-)
+) {
+    data class ServerSettings(
+        val url: URI, val authUser: String? = null, val authPassword: String? = null, val overridePageSize: Int? = null
+    ) {
+        fun configureRestTemplate() = RestTemplate().apply {
+            if (authUser != null && authPassword != null) {
+                interceptors.add(BasicAuthenticationInterceptor(authUser, authPassword))
+            }
+        }
+
+        fun configureHapiClient(fhirContext: FhirContext) = fhirContext.newRestfulGenericClient(
+            url.toString()
+        ).apply {
+            if (authUser != null && authPassword != null) {
+                this.registerInterceptor(BasicAuthInterceptor(authUser, authPassword))
+            }
+        }
+
+        fun configureValidationSupport(fhirContext: FhirContext) =
+            RemoteTerminologyServiceValidationSupport(fhirContext, url.toString()).apply {
+                if (authUser != null && authPassword != null) {
+                    this.addClientInterceptor(BasicAuthInterceptor(authUser, authPassword))
+                }
+            }
+    }
+}
--- src/main/kotlin/de/uksh/medic/fhirmarshal/services/ValidationSupportConfiguration.kt
+++ src/main/kotlin/de/uksh/medic/fhirmarshal/services/ValidationSupportConfiguration.kt
@@ -11,7 +11,6 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
-import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponentsBuilder
 import java.net.URI
 
@@ -24,20 +23,19 @@ class ValidationSupportConfiguration(
 ) {
 
     init {
-        validateServerIsFhir(properties.remoteTerminologyServer, "terminology")
-        validateServerIsFhir(properties.remoteStructureServer, "structure")
+        validateServerIsFhir(properties.remoteTerminologyServers, "terminology")
+        validateServerIsFhir(properties.remoteStructureServers, "structure")
     }
 
     fun configureChain() = ValidationSupportChain().apply {
         this.addValidationSupport(DefaultProfileValidationSupport(this@ValidationSupportConfiguration.fhirContext))
         this.addValidationSupport(CommonCodeSystemsTerminologyService(this@ValidationSupportConfiguration.fhirContext))
         this.addValidationSupport(InMemoryTerminologyServerValidationSupport(this@ValidationSupportConfiguration.fhirContext))
-        this.addValidationSupport(
-            RemoteTerminologyServiceValidationSupport(
-                this@ValidationSupportConfiguration.fhirContext,
-                properties.remoteTerminologyServer.toString()
-            )
-        )
+        properties.remoteTerminologyServers.forEach { (name, settings) ->
+            logger.info("Added terminology support via $name at ${settings.url}")
+            this.addValidationSupport(settings.configureValidationSupport(fhirContext))
+        }
+
         this.addValidationSupport(SnapshotGeneratingValidationSupport(this@ValidationSupportConfiguration.fhirContext))
         val prePopulatedValidationSupport =
             PrePopulatedValidationSupport(this@ValidationSupportConfiguration.fhirContext)
@@ -53,46 +51,59 @@ class ValidationSupportConfiguration(
         CachingValidationSupport(chain)
     }
 
-    private fun validateServerIsFhir(serverUrl: URI, role: String) {
-        val client = fhirContext.newRestfulGenericClient(serverUrl.toString())
-        try {
-            val capabilities = client.capabilities().ofType(CapabilityStatement::class.java).execute()
-            logger.info("Connected to $serverUrl; FHIR ${capabilities.fhirVersion}; Software '${capabilities.software.name} - ${capabilities.software.version}'")
-        } catch (e: Exception) {
-            throw ConfigurationError("could not connect to $role at $serverUrl", e)
+    private fun validateServerIsFhir(servers: Map<String, AppProperties.ServerSettings>, role: String) {
+        servers.forEach { (name, settings) ->
+            val client = settings.configureHapiClient(fhirContext)
+            try {
+                val capabilities = client.capabilities().ofType(CapabilityStatement::class.java).execute()
+                logger.info("Connected to $role server '$name' at ${settings.url}; FHIR ${capabilities.fhirVersion}; Software '${capabilities.software.name} - ${capabilities.software.version}'")
+            } catch (e: Exception) {
+                throw ConfigurationError("could not connect to $role server '$name' at ${settings.url}", e)
+            }
         }
+
     }
 
     /**
      * we are not using the HAPI FHIR client, since Firely Server handles paging differently to HAPI FHIR.
      * By following the urls manually, and parsing using JsonParser, this code works on HAPI-FHIR-JPA and Firely Server.
      */
-    private fun retrieveStructureDefinitions(): List<StructureDefinition> {
+    private fun retrieveStructureDefinitions(): List<StructureDefinition> =
+        properties.remoteStructureServers.flatMap { (name, settings) ->
+            retrieveStructureDefinitions(name, settings)
+        }.also {
+            logger.info("Finished structure definition retrieval with ${it.size} structure definitions from ${properties.remoteStructureServers.size} servers")
+        }
+
+    private fun retrieveStructureDefinitions(
+        serverName: String,
+        server: AppProperties.ServerSettings
+    ): MutableList<StructureDefinition> {
         val structureDefinitions = mutableListOf<StructureDefinition>()
-        val startUri = UriComponentsBuilder.fromUri(properties.remoteStructureServer).apply {
+        val pageSize = server.overridePageSize ?: properties.retrievalPageSize
+        val startUri = UriComponentsBuilder.fromUri(server.url).apply {
             pathSegment("StructureDefinition")
-            queryParam("_count", properties.retrievalPageSize)
+            queryParam("_count", pageSize)
             queryParam("_total", "accurate") // this might be ignored by some servers
             queryParam("status", "active")
         }.build().toUri()
-        logger.info("Starting StructureDefinition retrieval (page size=${properties.retrievalPageSize}) at: $startUri")
+        logger.info("Starting StructureDefinition retrieval from $serverName (page size=$pageSize) at: $startUri")
         var currentUri: URI? = startUri
-        val template = RestTemplate()
+        val template = server.configureRestTemplate()
         var pageCount = 0
         while (currentUri != null) {
             val result = template.getForEntity(currentUri, String::class.java).let { entity ->
                 fhirContext.newJsonParser().parseResource(Bundle::class.java, entity.body)
             }
-            logger.info("Retrieved page ${++pageCount} of StructureDefinitions with ${result.entry.size} entries from $currentUri")
-            if (pageCount == 0) logger.info("Server reported total=${result.total}")
+            logger.debug("Retrieved page ${++pageCount} of StructureDefinitions with ${result.entry.size} entries from $currentUri")
+            if (pageCount == 0) logger.debug("Server reported total=${result.total}")
             structureDefinitions.addAll(result.entry.mapNotNull { it.resource as? StructureDefinition })
             currentUri = result.getLink("next")?.let { nextLink ->
                 URI.create(nextLink.url)
             } ?: null.also {
-                logger.debug("Finished bundle retrieval from ${properties.remoteStructureServer}")
+                logger.info("Finished bundle retrieval from $serverName, got ${structureDefinitions.size} resources")
             }
         }
-
         return structureDefinitions
     }
 }
